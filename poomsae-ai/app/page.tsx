81ad71f86788a325e2ad7e81067c942e294ead57
Michael Hu <hh3480@nyu.edu>
2026-03-06 11:33:56 +0800

    create basic front page

--- poomsae-ai/app/page.tsx
+++ poomsae-ai/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello World!
+    <div className="flex min-h-screen flex-col items-center justify-center p-8">
+      <div className="flex flex-col items-center gap-8">
+        <div className="text-2xl font-bold font-sans">Upload Video</div>
+        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+          Upload
+        </button>
+      </div>
     </div>
   );
 }
